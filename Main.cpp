5f5f332be5682e88210805bce38a5a1e17af0ce4
Rohan Nolan Lasrado <rohanlasrado2001@gmail.com>
2021-11-17 22:23:38 +0530

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+//#include "InteractivePrompt.h"
+//#include "BW.cu"
+
+
+//
+
+
+#include "BW.h"
+
+auto main() -> int
+{
+	//InteractivePrompt prompt;
+	//prompt.promptLoop();
+
+	callBW();
+}
---
+++ kernels.h
@@ -0,0 +1,14 @@
+#ifndef BLACKWHITE
+#define BLACKWHITE
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+#include <stdio.h>
+#include "device_launch_parameters.h"
+#include "Dispatch.h"
+
+void callBW();
+
+
+#endif
