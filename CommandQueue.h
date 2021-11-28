e62f69675780065902229f8e0b526971dbce4884
Rohan Nolan Lasrado <54094843+lasradorohan@users.noreply.github.com>
2021-11-28 18:44:24 +0530

    Updated import to renamed ImageCommands.h

--- CommandQueue.h
+++ CommandQueue.h
@@ -4,39 +4,11 @@
 #include <list>
 #include <memory>
 #include <string>
-#include "kernels.h"
-//class Command {
-//public:
-//	virtual auto toString() -> const std::string = 0;
-//};
-//
-//class TempCommand : public Command {
-//	float radius;
-//public:
-//	TempCommand(float radius) : radius(radius) {
-//	}
-//	~TempCommand(){}
-//	auto toString() -> const std::string {
-//		return "TemporaryCommandv2(" + std::to_string(radius) + ")";
-//	}
-//};
-//
-//class Temp2Command : public Command {
-//	float height, width;
-//public:
-//	Temp2Command(float height, float width) : height(height), width(width) {}
-//	~Temp2Command() {}
-//	auto toString() -> const std::string {
-//		return "TemporaryCommandv2(" + std::to_string(height) + ", " + std::to_string(width) + ")";
-//	}
-//};
+#include "ImageCommands.h"
 
 class CommandQueue {
 	std::list<std::unique_ptr<ImageCommand>> queue;
 public:
-	/*auto addCommand(std::unique_ptr<ImageCommand> pCommand) -> void {
-		queue.push_back(pCommand);
-	}*/
 	template <class T, class... _Types>
 	auto addCommand(_Types&&... _Args) {
 		queue.push_back(std::make_unique<T>(std::forward<_Types>(_Args)...));
